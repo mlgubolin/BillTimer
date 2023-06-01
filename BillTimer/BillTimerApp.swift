084111f6ca90f7fe902a2ee4a414453bb72048df
Maria Lucia Gubolin <mluciagubolin@gmail.com>
2023-06-01 12:22:02 -0300

    Initial Commit

---
+++ BillTimer/BillTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BillTimerApp.swift
+//  BillTimer
+//
+//  Created by Maria Lucia Gubolin on 01/06/23.
+//
+
+import SwiftUI
+
+@main
+struct BillTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
