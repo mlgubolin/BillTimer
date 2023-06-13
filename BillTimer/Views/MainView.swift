70ea0976b787f669892fdb445dd6c8485f1db23b
Maria Lucia Gubolin <mlgubolin@Charlottes-MacBook-Pro.local>
2023-06-13 12:27:22 -0300

    test

--- BillTimer/Views/MainView.swift
+++ BillTimer/Views/MainView.swift
@@ -8,13 +8,47 @@
 import SwiftUI
 
 struct MainView: View {
+    @State var isShowMenu: Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ZStack {
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                    .disabled(self.isShowMenu)
+                    .toolbar{
+                        ToolbarItem (placement: .navigationBarTrailing){
+                            Button{
+                                withAnimation{
+                                    self.isShowMenu.toggle()
+                                }
+                            } label:{
+                                Image(systemName: "gear")
+                            }
+                        }
+                        ToolbarItem (placement: .navigationBarLeading){
+                            Button{
+                                withAnimation{
+                                    self.isShowMenu.toggle()
+                                }
+                            } label:{
+                                Image(systemName: "plus")
+                            }
+                        }
+                    }
+                if self.isShowMenu {
+                    MenuView()
+                        .transition(.move(edge: .bottom))
+                        .padding(.top,100)
+                }
+            }
+        }
+        .navigationBarTitle("side menu")
     }
 }
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
+//        MainView()
         MainView()
+            .previewDevice("iPhone SE (3rd generation)")
     }
 }
